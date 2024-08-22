62571d641b7fd55a52fc5d4c79ddebbc9b9027c1
Fabio Battaglia <tabaglio@posteo.net>
2024-08-22 20:02:16 +0200

    Added some tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -17,3 +17,11 @@ def ic_definition_27C2001() -> ICDefinition:
 @pytest.fixture
 def ic_definition_PAL12x6() -> ICDefinition:
     return ICLoader.extract_definition_from_file('examples/PAL12x6.toml')
+
+@pytest.fixture
+def filename_randomdata_1k() -> str:
+    return 'tests/data/random_1K.bin'
+
+@pytest.fixture
+def filename_randomdata_1_5k() -> str:
+    return 'tests/data/random_1.5K.bin'
---
+++ tests/test_file_utils.py
@@ -0,0 +1,32 @@
+"""Tests for IC definitions"""
+
+# pylint: disable=wrong-import-position,wrong-import-order
+
+import sys
+from typing import List
+sys.path.insert(0, './src') # Make VSCode happy...
+
+import pytest
+
+import dpdumperlib.io.file_utils as FileUtils
+
+def test_loading_1K(filename_randomdata_1k):
+    data_1B: List[int] = FileUtils.load_file_data(filename_randomdata_1k, 1)
+    assert(len(data_1B) == 1024)
+    
+    data_2B: List[int] = FileUtils.load_file_data(filename_randomdata_1k, 2)
+    assert(len(data_2B) == 512)
+
+    data_4B: List[int] = FileUtils.load_file_data(filename_randomdata_1k, 4)
+    assert(len(data_4B) == 256)
+
+    data_8B: List[int] = FileUtils.load_file_data(filename_randomdata_1k, 8)
+    assert(len(data_8B) == 128)
+
+    # Size of file is not divisible by 3
+    with pytest.raises(Exception):
+       FileUtils.load_file_data(filename_randomdata_1k, 3)
+
+def test_loading_1_5K(filename_randomdata_1_5k):
+    data_1_5B: List[int] = FileUtils.load_file_data(filename_randomdata_1_5k, 3)
+    assert(len(data_1_5B) == 512)
